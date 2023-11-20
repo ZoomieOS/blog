2758e555b67b3b6664580894b15471028328f736
Oleg Boreiko <oleg.boreiko@idfinance.com>
2023-11-20 23:55:45 +0100

    feat(tech): add babel and extracted translations

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,12 +3,12 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-    const { isDev} = options;
+    const {isDev} = options;
 
     const typeScriptLoader = {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
     };
 
     const svgLoader = {
@@ -17,37 +17,57 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     }
 
     const fileLoader = {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-                {
-                    loader: 'file-loader',
-                },
-            ],
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
     };
 
     const cssLoader = {
-            test: /\.s[ac]ss$/i,
-            use: [
-                isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader',
-                    options: {
-                        modules: {
-                            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                            localIdentName: isDev
-                                ? '[path][name]__[local]--[hash:base64:5]'
-                                : '[hash:base64:8]'
-                        },
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
                     },
                 },
-                "sass-loader",
-            ],
+            },
+            "sass-loader",
+        ],
     };
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    ["i18next-extract",
+                        {
+                            "locales": ["en", "ru"],
+                            "keyAsDefaultValue": true
+                        }
+                    ]
+                ]
+            }
+        }
+    }
+
     return [
-        typeScriptLoader,
-        cssLoader,
         svgLoader,
-        fileLoader
+        fileLoader,
+        babelLoader,
+        typeScriptLoader,
+        cssLoader
     ]
 }
