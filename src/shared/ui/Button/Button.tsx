abe88a1380e68ce1bd92a6d3aee988c37b4d8cf6
Oleg Boreiko <oleg.boreiko@idfinance.com>
2023-11-26 02:06:10 +0100

    feat(tech): fix jest issues, add one test

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,6 +1,6 @@
-import { classNames } from 'shared/lib/classNames/classNames'
 import { type ButtonHTMLAttributes, type FC } from 'react'
 import cls from './Button.module.scss'
+import { classNames } from 'shared/lib/classNames/classNames'
 
 export enum ThemeButton {
   CLEAR = 'clear',
